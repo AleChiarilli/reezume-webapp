05cbd448f19b299c9fd853e19de732f7a991061e
AleChiarilli <alejandrochiarilli@gmail.com>
2023-09-23 14:53:46 +0200

    feat: form step 4

--- src/app/form/additionalData/page.tsx
+++ src/app/form/additionalData/page.tsx
@@ -5,71 +5,39 @@ export default function additionalData() {
   return (
     <div className="flex flex-col justify-center align-middle">
       <div className="w-56 mt-5 text-white text-base font-normal leading-relaxed">
-        Tell about the person behind the screen
+        Tell about the person behind the screen...
       </div>
       <Form.Root>
-        <Form.Field name="position" className="pt-6 pb-5">
+        <Form.Field name="hobbies" className="pt-6 pb-5">
           <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-            Position
+            Hobbies
           </Form.Label>
           <Form.Control asChild>
             <input
               className="box-border w-full bg-transparent shadow-white inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white"
               type="text"
               required
-              placeholder="Project Manager"
+              placeholder="Reading, sports, movies..."
             />
           </Form.Control>
         </Form.Field>
-        <Form.Field name="Company" className="pb-5">
+        <Form.Field name="short-presentation" className="pb-5">
           <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-            Company
+            Short presentation
           </Form.Label>
           <Form.Control asChild>
             <input
-              className="box-border w-full bg-transparent shadow-white inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white"
+              className="box-border w-full bg-transparent shadow-white inline-flex h-[90px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white"
               type="text"
               required
-              placeholder="Hewlett-Packard"
+              placeholder="What moves you on?"
             />
           </Form.Control>
         </Form.Field>
-        <div className="flex justify-between">
-          {" "}
-          <Form.Field name="start-date" className="pb-2">
-            <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-              Start Date
-            </Form.Label>
-            <Form.Control asChild>
-              <input
-                className="box-border bg-transparent shadow-white inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white"
-                type="date"
-                required
-              />
-            </Form.Control>
-          </Form.Field>
-          <Form.Field name="end-date" className="pb-5">
-            <Form.Label className="text-[15px] font-medium leading-[35px] text-white">
-              End Date
-            </Form.Label>
-            <Form.Control asChild>
-              <input
-                className="box-border bg-transparent shadow-white inline-flex h-[35px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none hover:shadow-[0_0_0_1px_black] focus:shadow-[0_0_0_2px_black] selection:color-white"
-                type="date"
-                required
-              />
-            </Form.Control>
-          </Form.Field>
-        </div>
-
-        {/* éste button no está dentro del form.submit */}
-        <div className="flex justify-between">
-          <button className="box-border bg-gray-400 text-white shadow-blackA7 hover:bg-gray-300 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
-            Add another
-          </button>
+        <div className="w-full flex justify-end">
           <Form.Submit asChild>
             <button className="box-border bg-violet-500 text-white shadow-blackA7 hover:bg-violet-400 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[10px]">
-              Next
+              Done
             </button>
           </Form.Submit>
         </div>
