bc7292025c1cde867568b04bc409986fe16ff394
AleChiarilli <alejandrochiarilli@gmail.com>
2023-09-24 02:26:59 +0200

    feat: navigate with stepper

--- src/app/form/components/StepElement.tsx
+++ src/app/form/components/StepElement.tsx
@@ -1,35 +1,60 @@
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 function StepElement({
   children,
   isActive,
+  onClick,
 }: {
   children: React.ReactNode;
   isActive: boolean;
+  onClick: () => void;
 }) {
   const styles =
-    "w-12 h-12 py-3 bg-rose-400 rounded-[999px] border border-zinc-200 justify-center items-center inline-flex";
+    "hover:cursor-pointer w-12 h-12 py-3 bg-rose-400 rounded-[999px] border border-zinc-200 justify-center items-center inline-flex";
 
   return (
-    <div className={`${styles} ${isActive ? "" : "bg-opacity-60"}`}>
+    <div
+      onClick={onClick}
+      className={`${styles} ${isActive ? "" : "bg-opacity-60"}`}
+    >
       {children}
     </div>
   );
 }
 
 export default function Stepper() {
+  const router = useRouter();
   const pathname = usePathname();
+
+  const goToStepOne = () => router.push("/form");
+  const goToStepTwo = () => router.push("/form/professionalExperience");
+  const goToStepThree = () => router.push("/form/educationData");
+  const goToStepFour = () => router.push("/form/additionalData");
 
   return (
     <div className="flex justify-between pt-36 w-72 text-white font-bold drop-shadow-lg">
-      <StepElement isActive={pathname === "/form"}>1</StepElement>
-      <StepElement isActive={pathname === "/form/professionalExperience"}>
+      <StepElement onClick={goToStepOne} isActive={pathname === "/form"}>
+        1
+      </StepElement>
+      <StepElement
+        onClick={goToStepTwo}
+        isActive={pathname === "/form/professionalExperience"}
+      >
         2
       </StepElement>
-      <StepElement isActive={pathname === "/form/educationData"}>3</StepElement>
-      <StepElement isActive={pathname === "/form/additionalData"}>
+      <StepElement
+        onClick={goToStepThree}
+        isActive={pathname === "/form/educationData"}
+      >
+        3
+      </StepElement>
+      <StepElement
+        onClick={goToStepFour}
+        isActive={pathname === "/form/additionalData"}
+      >
         4
       </StepElement>
     </div>
