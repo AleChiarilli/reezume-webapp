275ef2c5b27e53222278393d6b14c5f6d240cf6d
AleChiarilli <alejandrochiarilli@gmail.com>
2023-09-24 04:49:11 +0200

    WIP: data persistence

--- src/app/form/actions.ts
+++ src/app/form/actions.ts
@@ -1,6 +1,35 @@
-import { redirect } from 'next/navigation'
+"use server";
+import { redirect } from "next/navigation";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
-export default async function createPersonalData(formData: FormData) {
-    console.log(formData.get("email"));
-    redirect("/form/professionalExperience");
+export async function createPersonalData(formData: FormData) {
+  const schema = z.object({
+    name: z.string(),
+    profession: z.string(),
+    location: z.string(),
+    email: z.string(),
+    phoneNumber: z.string(),
+    summary: z.string(),
+  });
+
+  const prisma = new PrismaClient();
+
+  const parsedForm = schema.parse({
+    name: formData.get("name"),
+    profession: formData.get("profession"),
+    location: formData.get("location"),
+    email: formData.get("email"),
+    phoneNumber: formData.get("phone-number"),
+    summary: formData.get("summary"),
+  });
+
+  await prisma.personalInformation.create({ data: parsedForm });
+
+  redirect("/form/professionalExperience");
+}
+
+export async function getPersonalData() {
+  const prisma = new PrismaClient();
+  return prisma.personalInformation.findFirst();
 }
